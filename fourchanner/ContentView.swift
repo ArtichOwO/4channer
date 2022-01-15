899a585a4668e646ae5e1dd9e88756d85927a77d
ArtichautCosmique <leo.grange@icloud.com>
2022-01-16 00:42:33 +0100

    feat: NavView in ContentView

--- fourchanner/ContentView.swift
+++ fourchanner/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("hey")
+            
+            .navigationBarTitle("4channer")
+            .navigationBarItems(trailing:
+                HStack {
+                    ForEach(0..<4) { _ in
+                        Image(systemName: "leaf")
+                            .foregroundColor(Color.green)
+                    }
+                }
+            )
+        }
     }
 }
 
