31923a16083194a605300e7bf3952ef4d220731f
ArtichautCosmique <leo.grange@icloud.com>
2022-01-16 00:17:13 +0100

    git: Initial Commit

---
+++ fourchanner/fourchannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fourchannerApp.swift
+//  fourchanner
+//
+//  Created by Artichaut on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct fourchannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
